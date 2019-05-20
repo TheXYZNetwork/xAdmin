ffb0b10dcf0d26afc0892ba344ae814e59f270e3
Owain Jones <owain.jones74@gmail.com>
2019-05-20 12:21:04 +0100

    cleaned up the config with better comments

--- lua/xadmin/_config/sh_config.lua
+++ lua/xadmin/_config/sh_config.lua
@@ -1,42 +1,50 @@
--- Ensure you go to xadmin/core/sv_db.lua and set up your SQL credentials
--- If you're running multiple servers, I also suggest going to autorun/xadmin_init.lua and setting up unique names for each server
+/*=============
+xAdmin Config
+=============*/
+-- Here is the config for xAdmin. You can create ranks, set needed power levels and even edit the ban message.
+-- Ensure you go to xadmin/core/sv_db.lua and set up your SQL credentials.
+-- If you're running multiple servers, I also suggest going to autorun/xadmin_init.lua and setting up unique names for each server. Else they will share info.
+-- You can find a "Getting Started" guide here: https://github.com/OwjoTheGreat/xadmin/wiki/Getting-Started
+-- If you want to change the needed power level of the command you can find that in the xadmin/commands folder. See the above guide (Getting Started) for a detailed guide on how that works.
+
+
+/*===========
+Ranks Config
+=============*/
+-- These are the ranks that users can be. The first argument is the rank name and the 2nd is the power level.
 
 -- Staff ranks
 xAdmin.Core.RegisterGroup("superadmin", 100)
-xAdmin.Core.RegisterGroup("senior-admin", 90)
 xAdmin.Core.RegisterGroup("admin", 80)
-xAdmin.Core.RegisterGroup("jr-admin", 70)
-xAdmin.Core.RegisterGroup("senior-moderator", 60)
 xAdmin.Core.RegisterGroup("moderator", 50)
-xAdmin.Core.RegisterGroup("jr-mod", 40)
-xAdmin.Core.RegisterGroup("trial-mod", 30)
 -- Paid ranks
-xAdmin.Core.RegisterGroup("VIP+", 20)
+xAdmin.Core.RegisterGroup("vip+", 20)
 xAdmin.Core.RegisterGroup("vip", 10)
 -- Base rank
 xAdmin.Core.RegisterGroup("user", 0)
 
-
--- The default group
+-- The default group that a user is given on first join.
 xAdmin.Config.DefaultGroup = "user"
 
+-- The power levels needed to be superadmin/admin
+xAdmin.Config.Superadmin = 100 -- Superadmin
+xAdmin.Config.Admin = 80 -- Admin
 
--- The power level needed to be superadmin/admin
-xAdmin.Config.Superadmin = 100
-xAdmin.Config.Admin = 80
-
-
--- What power-level can see admin chat?
+-- The power level that can see admin chat
 xAdmin.Config.AdminChat = 30
 -- The color of admin chat
 xAdmin.Config.AdminChatColor = Color(0,150,255)
 
 
+/*===============
+Foramtting Config
+=================*/
+
 -- The chat prefix
 xAdmin.Config.Prefix = "!"
 
-
 -- The ban message
+-- The %s formatting order is: Banning admin, time left, ban reason
 xAdmin.Config.BanFormat = [[------------
 
 -- Banned --
