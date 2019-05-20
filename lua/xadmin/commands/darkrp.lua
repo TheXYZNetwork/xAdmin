df015cf9613be54a3bfbfff0391140baba4b2a96
Owain Jones <owain.jones74@gmail.com>
2019-05-20 12:54:52 +0100

    added basic darkrp commands

---
+++ lua/xadmin/commands/darkrp.lua
@@ -0,0 +1,72 @@
+--- #
+--- # ADDMONEY
+--- #
+xAdmin.Core.RegisterCommand("addmoney", "[DarkRP] Gives the target X money", 100, function(admin, args)
+	if not args or not args[1] then return end
+	if not DarkRP then return end
+
+	if not args[2] or not tonumber(args[2]) or tonumber(args[2]) <= 0 then
+		xAdmin.Core.Msg({Color(46, 170, 200), "[xAdmin] ", Color(255, 255, 255), "Please provid a valid amount you want to add to the target"}, admin)
+		return
+	end
+
+	args[2] = tonumber(args[2])
+
+	local target = xAdmin.Core.GetUser(args[1], admin)
+	if not IsValid(target) then
+		xAdmin.Core.Msg({Color(46, 170, 200), "[xAdmin] ", Color(255, 255, 255), "Please provide a valid target. The following was not recognised: "..args[1]}, admin)
+		return
+	end
+
+	xAdmin.Core.Msg({admin, " has added ", DarkRP.formatMoney(args[2]), " to ", target})
+	target:addMoney(args[2])
+end)
+
+--- #
+--- # REMOVEMONEY
+--- #
+xAdmin.Core.RegisterCommand("removemoney", "[DarkRP] Takes X money from the target", 100, function(admin, args)
+	if not args or not args[1] then return end
+	if not DarkRP then return end
+
+	if not args[2] or not tonumber(args[2]) or tonumber(args[2]) <= 0 then
+		xAdmin.Core.Msg({Color(46, 170, 200), "[xAdmin] ", Color(255, 255, 255), "Please provid a valid amount you want to add to the target"}, admin)
+		return
+	end
+
+	args[2] = tonumber(args[2])
+
+	local target = xAdmin.Core.GetUser(args[1], admin)
+	if not IsValid(target) then
+		xAdmin.Core.Msg({Color(46, 170, 200), "[xAdmin] ", Color(255, 255, 255), "Please provide a valid target. The following was not recognised: "..args[1]}, admin)
+		return
+	end
+
+	xAdmin.Core.Msg({admin, " has taken ", DarkRP.formatMoney(args[2]), " from ", target})
+	target:addMoney(-args[2])
+end)
+
+
+--- #
+--- # SETMONEY
+--- #
+xAdmin.Core.RegisterCommand("setmoney", "[DarkRP] Sets the target's money to X", 100, function(admin, args)
+	if not args or not args[1] then return end
+	if not DarkRP then return end
+
+	if not args[2] or not tonumber(args[2]) or tonumber(args[2]) <= 0 then
+		xAdmin.Core.Msg({Color(46, 170, 200), "[xAdmin] ", Color(255, 255, 255), "Please provid a valid amount you want to add to the target"}, admin)
+		return
+	end
+
+	args[2] = tonumber(args[2])
+
+	local target = xAdmin.Core.GetUser(args[1], admin)
+	if not IsValid(target) then
+		xAdmin.Core.Msg({Color(46, 170, 200), "[xAdmin] ", Color(255, 255, 255), "Please provide a valid target. The following was not recognised: "..args[1]}, admin)
+		return
+	end
+
+	xAdmin.Core.Msg({admin, " has set ", target, "'s money to ", DarkRP.formatMoney(args[2])})
+	target:addMoney(-target:getDarkRPVar("money") + args[2]) -- Seems that DarkRP doesn't have a setMonet class?
+end)
