a8ff9ddb3987fe7f21a755e726dae9a873b7960f
roni_sl <rustemkam@me.com>
2019-07-22 12:28:18 -0700

    Move mysql config to configs folder, fix for using sqlite

---
+++ lua/xadmin/_config/sv_mysql_config.lua
@@ -0,0 +1,7 @@
+xAdmin.Database.UseMySQL = false
+xAdmin.Database.Cerds = {}
+xAdmin.Database.Cerds.ip = "1.1.1.1"
+xAdmin.Database.Cerds.user = "user"
+xAdmin.Database.Cerds.password = "Pa$$W0rd"
+xAdmin.Database.Cerds.database = "xadmin_master"
+xAdmin.Database.Cerds.port = 3306
